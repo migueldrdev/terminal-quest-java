b32c28560ddb343bb1c1f0a2c7447fe421312627
Segundo Miguel Delgado Ruiz <88801050+migueldrdev@users.noreply.github.com>
2025-12-01 20:58:30 -0500

    Actualizar Estadisticas.java

--- src/modelo/Estadisticas.java
+++ src/modelo/Estadisticas.java
@@ -2,6 +2,4 @@ package modelo;
 
 import java.io.Serializable;
 
-// 1. RECORD (Semana 17): Para datos inmutables como las coordenadas o stats base
-// Usamos esto en lugar de una clase con 20 getters y setters.
 public record Estadisticas(int fuerza, int defensa, int velocidad) implements Serializable {}
